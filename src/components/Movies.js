9b8cbfa2573fc478a7b6f98f7fddb68bcc1fca34
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-04 16:25:29 +0200

    Change movies view to a table with error and loading handling

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {fetchWithApiKey} from '../utils/fetchExtended'
+import { fetchWithApiKey } from '../utils/fetchExtended'
 import Loading from './Loading'
 import Error from './Error'
 
@@ -11,6 +11,7 @@ class Movies extends Component {
       searchName: '',
       searchYear: '',
       searchPage: 1,
+      maxPages: 1,
       movies: {
         list: [],
         loading: false,
@@ -23,21 +24,24 @@ class Movies extends Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  fetchMovies (title, year, page) {
-    this.setState({loading: true})
+  fetchMovies () {
+    this.setState({movies: {...this.state.movies, loading: true}})
 
-    fetchWithApiKey({s: title, y: year, page: page})
+    fetchWithApiKey({s: this.state.searchName, y: this.state.searchYear, page: this.state.searchPage})
       .then(response => {
         return response.json()
       })
       .then(responseJson => {
-        if (!responseJson.Response)
+        if (responseJson.Response === 'False')
           throw responseJson.Error
 
-        this.setState({movies: {...this.state.movies, list: responseJson.Search}})
+        this.setState({
+          maxPages: Math.ceil(responseJson.totalResults / 10),
+          movies: {...this.state.movies, list: responseJson.Search},
+        })
       })
       .catch(error => {
-        this.setState({movies: {...this.state.movies, error: error.error}})
+        this.setState({movies: {...this.state.movies, error: error}})
       })
       .finally(() => {
         this.setState({movies: {...this.state.movies, loading: false}})
@@ -45,7 +49,7 @@ class Movies extends Component {
   }
 
   componentWillMount () {
-    // this.fetchMovies()
+
   }
 
   handleNameChange (event) {
@@ -57,30 +61,70 @@ class Movies extends Component {
   }
 
   handleSubmit (event) {
-    this.fetchMovies(this.state.searchName, this.state.searchYear, this.state.searchPage)
+    this.fetchMovies()
     event.preventDefault()
+  }
+
+  changePage (value) {
+    if (this.state.searchPage + value >= 1 && this.state.searchPage + value <= this.state.maxPages) {
+      this.setState({...this.state, searchPage: this.state.searchPage + value}, () => this.fetchMovies())
+    }
   }
 
   render () {
     return (
-      <div>
+      <div className='container-fluid'>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.searchName} onChange={this.handleNameChange}/>
-          </label>
-          <label>
-            Year:
-            <input type="number" value={this.state.searchYear} onChange={this.handleYearChange}/>
-          </label>
-          <input type="submit" value="Submit"/>
+          <div className='form-group'>
+            <label>Name:</label>
+            <input className='form-control' type='text' value={this.state.searchName} onChange={this.handleNameChange}
+                   style={{maxWidth: '50%'}}/>
+          </div>
+          <div className="form-group">
+            <label>Year:</label>
+            <input className='form-control' type='number' value={this.state.searchYear} onChange={this.handleYearChange}
+                   style={{maxWidth: '15%'}}/>
+          </div>
+          <input className='btn btn-primary' type='submit' value='Submit'/>
         </form>
 
-        <h2>Movies: </h2>
+        <hr/>
+
+        <b>Movies: </b>
+
         <Loading loading={this.state.movies.loading}/>
         <Error error={this.state.movies.error}/>
+
+        {this.state.movies.list && this.state.maxPages !== 1 && <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            <li className="page-item">
+              <button className="page-link" onClick={() => this.changePage(-1)}>Previous</button>
+            </li>
+            <li className="page-item">
+              <button className="page-link" onClick={() => this.changePage(1)}>Next</button>
+            </li>
+          </ul>
+        </nav>}
+
+        <table className="table">
+          <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Year</th>
+            <th scope="col">Poster</th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.state.movies.list && this.state.movies.list.map(movie => <tr key={movie.imdbID}>
+              <th scope="row">{movie.Title}</th>
+              <td>{movie.Year}</td>
+              <td>{movie.Poster && movie.Poster !== 'N/A' &&
+              <img alt='Poster' src={movie.Poster} style={{width: '20%'}}/>}</td>
+            </tr>,
+          )}
+          </tbody>
+        </table>
         <ul>
-          {this.state.movies.list.map(movie => <li key={movie.imdbID}>{movie.Title}</li>)}
         </ul>
       </div>
     )
