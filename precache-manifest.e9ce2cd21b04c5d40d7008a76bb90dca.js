c1da3e9addcd726671db8955d8771754e352807d
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-13 15:51:38 +0200

    Updates

---
+++ precache-manifest.e9ce2cd21b04c5d40d7008a76bb90dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5f2908177bd6c25e709",
+    "url": "/static/css/main.8ee484d0.chunk.css"
+  },
+  {
+    "revision": "e5f2908177bd6c25e709",
+    "url": "/static/js/main.a5ff2590.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "411426e65d90f7583e35",
+    "url": "/static/css/2.f04eeece.chunk.css"
+  },
+  {
+    "revision": "411426e65d90f7583e35",
+    "url": "/static/js/2.9221fec9.chunk.js"
+  },
+  {
+    "revision": "93e54ba9f730e434b3ac3ef48a50b063",
+    "url": "/static/media/billy.93e54ba9.jpg"
+  },
+  {
+    "revision": "ab19b966ce59b15c034df2dcdb2efab4",
+    "url": "/index.html"
+  }
+];
