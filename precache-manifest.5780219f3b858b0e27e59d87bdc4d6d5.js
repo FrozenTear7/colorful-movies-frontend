5a4fdaec0d91ec77d13fa00850be25390ae2638d
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-13 16:10:54 +0200

    Updates

---
+++ precache-manifest.5780219f3b858b0e27e59d87bdc4d6d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4e0261f9c53daa865bd",
+    "url": "/colorful-movies-frontend/static/css/main.8ee484d0.chunk.css"
+  },
+  {
+    "revision": "f4e0261f9c53daa865bd",
+    "url": "/colorful-movies-frontend/static/js/main.fb02cf9c.chunk.js"
+  },
+  {
+    "revision": "0e5cecbb4b3062620479",
+    "url": "/colorful-movies-frontend/static/js/runtime~main.243cfbed.js"
+  },
+  {
+    "revision": "411426e65d90f7583e35",
+    "url": "/colorful-movies-frontend/static/css/2.f04eeece.chunk.css"
+  },
+  {
+    "revision": "411426e65d90f7583e35",
+    "url": "/colorful-movies-frontend/static/js/2.9221fec9.chunk.js"
+  },
+  {
+    "revision": "93e54ba9f730e434b3ac3ef48a50b063",
+    "url": "/colorful-movies-frontend/static/media/billy.93e54ba9.jpg"
+  },
+  {
+    "revision": "100da06572ce1c6a33fca9bfd8aac962",
+    "url": "/colorful-movies-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9ce2cd21b04c5d40d7008a76bb90dca.js"
+  "/colorful-movies-frontend/precache-manifest.5780219f3b858b0e27e59d87bdc4d6d5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/colorful-movies-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
