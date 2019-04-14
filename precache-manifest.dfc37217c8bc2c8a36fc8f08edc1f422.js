387decc96d932af53c23008add4cb133a3caeb17
FrozenTear7 <RainbowPablo7@gmail.com>
2019-04-14 14:31:22 +0200

    Updates

--- precache-manifest.dfc37217c8bc2c8a36fc8f08edc1f422.js
+++ precache-manifest.dfc37217c8bc2c8a36fc8f08edc1f422.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "a4dcea86b5c5dea22470",
-    "url": "/colorful-movies-frontend/static/css/main.8ee484d0.chunk.css"
+    "revision": "b6c1774e86ce8529f8c6",
+    "url": "/colorful-movies-frontend/static/css/main.ecf5dcf2.chunk.css"
   },
   {
-    "revision": "a4dcea86b5c5dea22470",
-    "url": "/colorful-movies-frontend/static/js/main.379c1457.chunk.js"
+    "revision": "b6c1774e86ce8529f8c6",
+    "url": "/colorful-movies-frontend/static/js/main.c4744a98.chunk.js"
   },
   {
     "revision": "0e5cecbb4b3062620479",
     "url": "/colorful-movies-frontend/static/js/runtime~main.243cfbed.js"
   },
   {
-    "revision": "411426e65d90f7583e35",
+    "revision": "2d9531b125596d45881d",
     "url": "/colorful-movies-frontend/static/css/2.f04eeece.chunk.css"
   },
   {
-    "revision": "411426e65d90f7583e35",
-    "url": "/colorful-movies-frontend/static/js/2.9221fec9.chunk.js"
+    "revision": "2d9531b125596d45881d",
+    "url": "/colorful-movies-frontend/static/js/2.780b1baa.chunk.js"
   },
   {
     "revision": "93e54ba9f730e434b3ac3ef48a50b063",
     "url": "/colorful-movies-frontend/static/media/billy.93e54ba9.jpg"
   },
   {
-    "revision": "689e79cf3adc5b6ab19327f6c2c4935e",
+    "revision": "6942c4a484a86796ea129b7e40a5ad43",
     "url": "/colorful-movies-frontend/index.html"
   }
 ];
