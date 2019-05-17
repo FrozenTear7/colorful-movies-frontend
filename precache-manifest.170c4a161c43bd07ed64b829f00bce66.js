df191f4fb5a6c68e695c0081b303d8986f2b4f87
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 17:53:36 +0000

    Deploy FrozenTear7/colorful-movies-frontend to github.com/FrozenTear7/colorful-movies-frontend.git:gh-pages

---
+++ precache-manifest.170c4a161c43bd07ed64b829f00bce66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cd0ba4908cc10e3638b",
+    "url": "/colorful-movies-frontend/static/css/main.dd4d4558.chunk.css"
+  },
+  {
+    "revision": "3cd0ba4908cc10e3638b",
+    "url": "/colorful-movies-frontend/static/js/main.b81810cf.chunk.js"
+  },
+  {
+    "revision": "0e5cecbb4b3062620479",
+    "url": "/colorful-movies-frontend/static/js/runtime~main.243cfbed.js"
+  },
+  {
+    "revision": "0fbd4abcb72168e728da",
+    "url": "/colorful-movies-frontend/static/css/2.b316e40f.chunk.css"
+  },
+  {
+    "revision": "0fbd4abcb72168e728da",
+    "url": "/colorful-movies-frontend/static/js/2.29f4c21c.chunk.js"
+  },
+  {
+    "revision": "93e54ba9f730e434b3ac3ef48a50b063",
+    "url": "/colorful-movies-frontend/static/media/billy.93e54ba9.jpg"
+  },
+  {
+    "revision": "9ee129cdc90a47c11bfba36018820b30",
+    "url": "/colorful-movies-frontend/index.html"
+  }
+];
