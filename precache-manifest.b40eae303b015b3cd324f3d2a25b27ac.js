9369fe9a151f4e5417353e5a58267fbd606eebd8
FrozenTear7 <RainbowPablo7@gmail.com>
2019-05-17 19:37:12 +0200

    Updates

---
+++ precache-manifest.b40eae303b015b3cd324f3d2a25b27ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e30bcbda81aeeee2d3c",
+    "url": "/colorful-movies-frontend/static/css/main.ea0a3daf.chunk.css"
+  },
+  {
+    "revision": "9e30bcbda81aeeee2d3c",
+    "url": "/colorful-movies-frontend/static/js/main.427b7718.chunk.js"
+  },
+  {
+    "revision": "0e5cecbb4b3062620479",
+    "url": "/colorful-movies-frontend/static/js/runtime~main.243cfbed.js"
+  },
+  {
+    "revision": "8e00596c2ce5fda8f22c",
+    "url": "/colorful-movies-frontend/static/css/2.f04eeece.chunk.css"
+  },
+  {
+    "revision": "8e00596c2ce5fda8f22c",
+    "url": "/colorful-movies-frontend/static/js/2.29f4c21c.chunk.js"
+  },
+  {
+    "revision": "93e54ba9f730e434b3ac3ef48a50b063",
+    "url": "/colorful-movies-frontend/static/media/billy.93e54ba9.jpg"
+  },
+  {
+    "revision": "ccba052291fd6ac11637911c00ee4819",
+    "url": "/colorful-movies-frontend/index.html"
+  }
+];
