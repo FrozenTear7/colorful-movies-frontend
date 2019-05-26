636afe282185efe5d7f04125ce3d405a63ba8a03
Paweł Mendroch <pawelmendroch7@gmail.com>
2019-05-26 16:39:23 +0200

    Add some more tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,11 +5,14 @@ const routes = {
     public: {
         panel: `${appUrlBase}/`,
         noMatch: `${appUrlBase}/404test`,
-    },
-    private: {
         movies: `${appUrlBase}/movies`,
+        movie: `${appUrlBase}/movies/tt1951264`,
         profile: `${appUrlBase}/profile`,
     },
+    // private: {
+    //     movies: `${appUrlBase}/movies`,
+    //     profile: `${appUrlBase}/profile`,
+    // },
 }
 
 let browser
@@ -18,7 +21,7 @@ let page
 beforeAll(async () => {
     browser = await puppeteer.launch({
             headless: true,
-            slowMo: 250,
+            slowMo: 100,
         },
     )
 
@@ -35,7 +38,53 @@ describe('Main panel test', () => {
     }, 1600000)
 })
 
-describe('404 Page', () => {
+// TODO: Change when authorization is implemented
+describe('Movies test', () => {
+    test('Fetching movies', async () => {
+        await page.goto(routes.public.movies)
+        await page.waitForSelector('.container-fluid')
+
+        await page.click('#root > div > div.container-fluid > form > input:nth-child(1)')
+        await page.type('#root > div > div.container-fluid > form > input:nth-child(1)', 'Fire')
+
+        await page.click('#root > div > div.container-fluid > form > input:nth-child(2)')
+        await page.type('#root > div > div.container-fluid > form > input:nth-child(2)', '2016')
+
+        await page.click('#root > div > div.container-fluid > form > input.btn.btn-primary')
+
+        await page.waitForSelector('#root > div > div.container-fluid > div > table > tbody > tr:nth-child(1)')
+
+        const tableRows = await page.$$('.table tbody')
+
+        const numberOfRows = tableRows.length
+        expect(numberOfRows).toBeGreaterThan(0)
+    }, 1600000)
+})
+
+// TODO: Change when authorization is implemented
+describe('Movie test', () => {
+    test('Fetching movie', async () => {
+        await page.goto(routes.public.movie)
+        await page.waitForSelector('#root > div > div.container-fluid > h1')
+
+        const title = await page.$eval('#root > div > div.container-fluid > h1', e => e.innerHTML)
+
+        expect(title).toBe('The Hunger Games: Catching Fire')
+    }, 1600000)
+
+    test('Rating', async () => {
+        await page.goto(routes.public.movie)
+        await page.waitForSelector('#root > div > div.container-fluid > div > div:nth-child(2) > div > button.btn-sm.btn-info.mr-2')
+
+        await page.click('#root > div > div.container-fluid > div > div:nth-child(2) > div > button.btn-sm.btn-info.mr-2')
+
+        const ratings = await page.$$('#root > div > div.container-fluid > div > div:nth-child(2) > div > ul > li > div')
+
+        expect(ratings.length).toBeGreaterThan(0)
+    }, 1600000)
+})
+
+describe('404 Page test', () => {
     test('404 page for nonexistent views', async () => {
         await page.goto(routes.public.noMatch)
         await page.waitForSelector('.container-fluid')
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
 import Panel from './core/Panel'
 import Movies from './movies/Movies'
 import NotFound from './utils/NotFound'
@@ -8,18 +8,22 @@ import Movie from './movies/Movie'
 import PrivateRoute from './utils/PrivateRoute'
 
 class Main extends Component {
-  render () {
-    return (
-      <Switch>
-        <Route exact path='/colorful-movies-frontend/' component={Panel}/>
-        <PrivateRoute exact path='/colorful-movies-frontend/movies' component={Movies}/>
-        <PrivateRoute path='/colorful-movies-frontend/movies/:imdbID' component={Movie}/>
-        <PrivateRoute path='/colorful-movies-frontend/profile' component={Profile}/>
-        <PrivateRoute path='/colorful-movies-frontend/users/:userid' component={Profile}/>
-        <Route component={NotFound}/>
-      </Switch>
-    )
-  }
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/colorful-movies-frontend/' component={Panel}/>
+                <Route exact path='/colorful-movies-frontend/movies' component={Movies}/>
+                <Route path='/colorful-movies-frontend/movies/:imdbID' component={Movie}/>
+                <Route path='/colorful-movies-frontend/profile' component={Profile}/>
+                <Route path='/colorful-movies-frontend/users/:userid' component={Profile}/>
+                {/*<PrivateRoute exact path='/colorful-movies-frontend/movies' component={Movies}/>*/}
+                {/*<PrivateRoute path='/colorful-movies-frontend/movies/:imdbID' component={Movie}/>*/}
+                {/*<PrivateRoute path='/colorful-movies-frontend/profile' component={Profile}/>*/}
+                {/*<PrivateRoute path='/colorful-movies-frontend/users/:userid' component={Profile}/>*/}
+                <Route component={NotFound}/>
+            </Switch>
+        )
+    }
 }
 
 export default Main
